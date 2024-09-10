faf42575d4f3cf485e728d8d3febfeffadaf6652
yuyawesleyan <ea21002@wesleyan.ac.jp>
2024-09-10 22:19:59 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Firebaseの設定情報をここに記述してください
+const firebaseConfig = {
+    apiKey: "AIzaSyDN9KQ50hwjlzFNc26aMOCS0H06JwggY68",
+    authDomain: "honkoku-hiroba-21400.firebaseapp.com",
+    projectId: "honkoku-hiroba-21400",
+    storageBucket: "honkoku-hiroba-21400.appspot.com",
+    messagingSenderId: "909469448032",
+    appId: "1:909469448032:web:8c62f7f2a978c711cd9005",
+    measurementId: "G-9747LKWGLN"
+};
+
+// Firebaseの初期化
+firebase.initializeApp(firebaseConfig);
